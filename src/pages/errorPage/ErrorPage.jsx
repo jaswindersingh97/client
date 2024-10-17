ad25cd0d1dc058e85f1d11faf1222afe180d791b
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-17 12:47:29 +0530

    error page

---
+++ src/pages/errorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './index.module.css';
+function ErrorPage() {
+  return (
+    <div className={styles.container}>
+      <h1>Error 404</h1>
+    </div>
+  )
+}
+
+export default ErrorPage
---
+++ src/pages/index.jsx
@@ -0,0 +1,3 @@
+import Home from './Home/Home';
+import ErrorPage from './errorPage/ErrorPage';
+export {Home,ErrorPage}
