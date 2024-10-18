5cc77f7c19ddebc074b25e5a3a121c352bf55ae0
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-18 08:29:35 +0530

    commit

--- src/pages/AnalyticsPage/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage/AnalyticsPage.jsx
@@ -1,10 +1,38 @@
 import styles from './AnalyticsPage.module.css';
 import React from 'react'
 
+function Item({item}){
+  const {name,count,color} = item;
+  return(
+    <div className={styles.Item}>
+      <span style={{background:color}}></span>
+      <p>{name}</p>
+      <h4>{count}</h4>
+    </div>
+  )
+}
+
 function AnalyticsPage() {
+  const Tasks =[
+    {name:"Backlog Tasks",color:"#90C4CC",count:16},
+    {name:"Low Priority",color:"#63C05B",count:16},
+    {name:"To-do Tasks",color:"#1E90FF",count:16},
+    {name:"Moderate Priority",color:"#18B0FF",count:16},
+    {name:"In-Progress Tasks",color:"#FFA500",count:16},
+    {name:"High Priority",color:"#FF2473",count:16},
+    {name:"Completed Tasks",color:"#32CD32",count:16},
+    {name:"Due Date Tasks",color:"#CF3636",count:16},
+  ];
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h3>Analytics</h3>
+      </div>
+      <div className={styles.body}>
+        {Tasks.map((item,index)=>(
+          <Item item={item} key={index}/>
+        ))}
+      </div>
     </div>
   )
 }
