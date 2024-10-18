5c076c54f57e9a13c3c342c6e6f14038d209c061
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-18 23:03:48 +0530

    Update ErrorPage.jsx

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from './Error.module.css';
+import React from 'react';
+import styles from './error.module.css';
 import { Link } from 'react-router-dom';
 function ErrorPage() {
   return (
