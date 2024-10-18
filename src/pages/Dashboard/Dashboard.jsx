12c2e78db958129cbdb2e12033426dfdf560cb72
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-18 09:43:19 +0530

    commit

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,36 @@
 import styles from './Dashboard.module.css';
-import React from 'react'
+import React, { useState } from 'react'
 
 function Dashboard() {
+  const user ={name:"jaswinder"}
+  const filterHandle = (e) => {
+    setSelectedValue(e.target.value)
+  }
+  const date = '12th Jan, 2024'
+  const Options = [
+    {value:1,title:'Today'},
+    {value:7,title:'This Week'},
+    {value:30,title:'This Month'},
+  ]
+  const [selectedValue,setSelectedValue] = useState(7);
   return (
-    <div>
-      hi
+    <div className={styles.container}>
+    <div className={styles.header}>
+      <h3>Welcome! {user.name}</h3>
+      <h4>{date}</h4>
     </div>
+    <div className={styles.subHeading}>
+      <h2>Board</h2>
+      <select defaultValue={selectedValue} onChange={filterHandle}>
+      {Options.map((item,index)=>(
+        <option key={index} value={item.value}>{item.title}</option>
+      ))}
+      </select>
+    </div>
+    <div className={styles.body}>
+
+    </div>
+  </div>
   )
 }
 
