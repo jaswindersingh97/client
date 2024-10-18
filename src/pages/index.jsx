ad1c8c69528cef231ab49b1eaec534b2e0d9acdc
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-18 23:22:38 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import Dashboard from './Dashboard/Dashboard';
 import Register from './Register/Register';
 import SignIn from './SignIn/SignIn';
-import ErrorPage from './ErrorPage/ErrorPage';
+import ErrorPage from './errorPage/ErrorPage';
 import SettingsPage from './SettingsPage/SettingsPage';
 import AnalyticsPage from './AnalyticsPage/AnalyticsPage';
 export {Dashboard,Register,SignIn,ErrorPage,SettingsPage,AnalyticsPage}
