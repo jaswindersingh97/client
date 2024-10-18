59e8c5d6d30df79b1ccef62cdaa5e59099b46ed3
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-18 23:08:40 +0530

    commit

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './error.module.css';
+import styles from './ErrorPage.module.css';
 import { Link } from 'react-router-dom';
 function ErrorPage() {
   return (
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import Dashboard from './Dashboard/Dashboard';
 import Register from './Register/Register';
 import SignIn from './SignIn/SignIn';
-import ErrorPage from './errorPage/ErrorPage';
+import ErrorPage from './ErrorPage/ErrorPage';
 import SettingsPage from './SettingsPage/SettingsPage';
 import AnalyticsPage from './AnalyticsPage/AnalyticsPage';
 export {Dashboard,Register,SignIn,ErrorPage,SettingsPage,AnalyticsPage}
