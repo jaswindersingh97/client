a21e36cd30f40f5c95901d3ca671251fb5afbdd4
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-20 13:57:21 +0530

    Update LeftNavLst.jsx

--- src/ComponentUtils/LeftNavLst.jsx
+++ src/ComponentUtils/LeftNavLst.jsx
@@ -1,14 +1,17 @@
 import {logo,Board,Analytics,Logout,Setting}  from './../assets/MainLayoutComponent';
+
 const headerelements =[
     {icon:logo,name:"Pro Manage",link:"/dashboard"},
   ];
-  const bodyelements = [
-    {icon:Board,name:"Board",link:"/dashboard"},
-    {icon:Analytics,name:"Analytics",link:"/analytics"},
-    {icon:Setting,name:"Settings",link:"/settings"},
-  ];
-  const footerelements = [
-    {icon:Logout,name:"Log out",onClick:()=>{openModal()}},
-  ];
+
+const bodyelements = [
+  {icon:Board,name:"Board",link:"/dashboard"},
+  {icon:Analytics,name:"Analytics",link:"/analytics"},
+  {icon:Setting,name:"Settings",link:"/settings"},
+];
+
+const footerelements = [
+  {icon:Logout,name:"Log out",onClick:()=>{openModal()}},
+];
   
 export {headerelements,bodyelements,footerelements};  
