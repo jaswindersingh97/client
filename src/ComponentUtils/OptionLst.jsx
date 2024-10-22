f0680aab336e8156b4ee2b22da38fb14b187ab56
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-22 13:00:05 +0530

    share button complete

--- src/ComponentUtils/OptionLst.jsx
+++ src/ComponentUtils/OptionLst.jsx
@@ -1,6 +1,20 @@
+import { toast } from "react-toastify";
 const handleclk = (id) => alert(id);    // dummy function for the options button
+
+const shareClk = (id) => {
+  const fullDomain = window.location.hostname + (window.location.port ? `:${window.location.port}` : '');
+  const link = `${fullDomain}/share/${id}` ;
+  navigator.clipboard.writeText(link)
+  .then(() => {
+    toast.success("Link copied to clipboard!");
+  })
+  .catch(err => {
+    toast.error("Failed to copy the link.");
+  });
+};
+
 const OptionsLst = [
-    { name: 'Share', onClick: handleclk },
+    { name: 'Share', onClick: shareClk },
     { name: 'Edit', onClick: handleclk },
     { name: 'Delete', color: '#CF3636', onClick: handleclk }
   ];
