010ac9b9888ceef6409a5f191307b5f32e099792
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-25 23:33:37 +0530

    Update AnalyticsPage.jsx

--- src/pages/AnalyticsPage/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage/AnalyticsPage.jsx
@@ -18,14 +18,14 @@ function AnalyticsPage() {
   const {token} = useContext(AppContext);
   const [taskList,setTaskList] = useState([]);
   const Tasks = [
-    { name: "Backlog Tasks", color: "#90C4CC", count: 0 },
-    { name: "Low Priority", color: "#63C05B", count: 0 },
-    { name: "To-do Tasks", color: "#1E90FF", count: 0 },
-    { name: "Moderate Priority", color: "#18B0FF", count: 0 },
-    { name: "In-Progress Tasks", color: "#FFA500", count: 0 },
-    { name: "High Priority", color: "#FF2473", count: 0 },
-    { name: "Completed Tasks", color: "#32CD32", count: 0 },
-    { name: "Due Date Tasks", color: "#CF3636", count: 0 }
+    { name: "Backlog Tasks", color: "#90C4CC", count: "" },
+    { name: "Low Priority", color: "#63C05B", count: "" },
+    { name: "To-do Tasks", color: "#1E90FF", count: "" },
+    { name: "Moderate Priority", color: "#18B0FF", count: "" },
+    { name: "In-Progress Tasks", color: "#FFA500", count: "" },
+    { name: "High Priority", color: "#FF2473", count: "" },
+    { name: "Completed Tasks", color: "#32CD32", count: "" },
+    { name: "Due Date Tasks", color: "#CF3636", count: "" }
   ];
   useEffect(() => {
     getTasks();
@@ -37,22 +37,20 @@ function AnalyticsPage() {
     } else if (task.status === 2) {
       Tasks[4].count++; 
     } else if (task.status === 3) {
-      Tasks[0].count++; // "Backlog Tasks"
+      Tasks[0].count++; 
     } else if (task.status === 4) {
-      Tasks[6].count++; // "Completed Tasks"
+      Tasks[6].count++; 
     }
-  
-    // Based on task.priority
     if (task.priority === 1) {
-      Tasks[1].count++; // "Low Priority"
+      Tasks[1].count++;
     } else if (task.priority === 2) {
-      Tasks[3].count++; // "Moderate Priority"
+      Tasks[3].count++;
     } else if (task.priority === 3) {
-      Tasks[5].count++; // "High Priority"
+      Tasks[5].count++;
     }
   
     if (task.dueDate) {
-      Tasks[7].count++; // "Due Date Tasks"
+      Tasks[7].count++;
     }    
   });
 
@@ -65,7 +63,6 @@ function AnalyticsPage() {
       },
     });
     setTaskList(response.data.response);
-    // setTaskData(response.data.response || []); // Ensure taskData is an array
   };
   return (
     <div className={styles.container}>
